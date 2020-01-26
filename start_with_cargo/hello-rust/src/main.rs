8a8f24e720fab92092d701b85ab60085ee319e9a
qalexball <zhuderen12345@icloud.com>
2020-01-27 00:18:36 +0800

    add trait examples

--- start_with_cargo/hello-rust/src/main.rs
+++ start_with_cargo/hello-rust/src/main.rs
@@ -1,7 +1,54 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
+mod my_lib;
+use my_lib::*;
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
 fn main() {
+    test_rustaceans();
+    test_summary();
+    test_largest();
+    test_pair();
+}
+
+fn test_pair() {
+    let pair = Pair {
+        x: String::from("a"),
+        y: String::from("b"),
+    };
+    let pair1 = Pair::new(1, 3);
+    pair.cmp_dispaly();
+    pair1.cmp_dispaly();
+}
+
+fn test_largest() {
+    let num_list = vec![2, 6, 8, 19, 3, 5];
+    println!("largest number is: {}", largest(&num_list));
+}
+
+fn test_summary() {
+    let tweet = Tweet {
+        username: String::from("chirs"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
+    notify(tweet);
+}
+
+fn test_rustaceans() {
     let stdout = stdout();
     let out = b"Hello fellow Rustaceans!";
     let width = 24;
---
+++ start_with_cargo/hello-rust/src/my_lib.rs
@@ -0,0 +1,67 @@
+use std::fmt::Display;
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("Read More from {}", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
+    // fn summarize(&self) -> String {
+    //     format!("{}: {}", self.username, self.content)
+    // }
+}
+
+pub fn notify(item: impl Summary) {
+    println!("Breaking news ... {}", item.summarize());
+}
+
+pub struct Pair<T> {
+    pub x: T,
+    pub y: T,
+}
+
+impl<T> Pair<T> {
+    pub fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    pub fn cmp_dispaly(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
